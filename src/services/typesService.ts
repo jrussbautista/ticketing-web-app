732b24b430cbce7165fa2efa57f2d438188a8de6
James Russel C. Bautista <jrussclay@gmail.com>
2022-10-26 11:31:39 +0800

    Add activate/deactivate ticket type

--- src/services/typesService.ts
+++ src/services/typesService.ts
@@ -1,8 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
-import { getTypes } from 'api/typesApi';
+import { deactivateType, activateType, getTypes } from 'api/typesApi';
 import { queryKeys } from 'app/constants';
+import queryClient from 'lib/queryClient';
 
 export const useTypes = (params: Record<string, any> = {}) => {
   return useQuery([queryKeys.TYPES, params], () => getTypes(params));
+};
+
+export const useDeactivateType = () => {
+  return useMutation({
+    mutationFn: deactivateType,
+    onSuccess: () => {
+      queryClient.invalidateQueries([queryKeys.TYPES]);
+    },
+  });
+};
+
+export const useActivateType = () => {
+  return useMutation({
+    mutationFn: activateType,
+    onSuccess: () => {
+      queryClient.invalidateQueries([queryKeys.TYPES]);
+    },
+  });
 };
